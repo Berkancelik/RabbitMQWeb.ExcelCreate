c40caba3d4d4ba76f5210f55273134b294ad9e19
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-21 10:08:25 +0300

    FilesController > UpLoad Method has been created

---
+++ RabbitMQWeb.ExcelCreate/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQWeb.ExcelCreate.Models;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.ExcelCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FilesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpLoad(IFormFile file, string userId, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+            {
+                var userFile = await _context.UserFiles.FirstAsync(x => x.Id == fileId);
+                var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+                using FileStream stream = new(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+                userFile.CreatedDate = System.DateTime.Now;
+                userFile.FilePath = filePath;
+                userFile.FileStatus = FileStatus.Completed;
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+        }
+    }
+}
